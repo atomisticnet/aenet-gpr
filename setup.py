da4c3c103180e5e6efbffb36bf0bff6bc6fd8899
In Won Yeu <yiw0121@snu.ac.kr>
2025-03-28 12:25:55 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = ['numpy',
+                    'pytorch',
+                    'dscribe',]
+
+packages = ['aenet_GPR',
+            'aenet_GPR.inout',
+            'aenet_GPR.src',
+            'aenet_GPR.util',]
+
+scripts = ['scripts/aenet_GPR.py', ]
+
+if __name__ == '__main__':
+
+    assert sys.version_info >= (3, 0), 'python>=3 is required'
+
+    with open('__init__.py', 'r') as init_file:
+        for line in init_file:
+            if "__version__" in line:
+                version = line.split()[2].strip('\"')
+                break
+
+setup(name='aenet_GPR',
+      description='Atomistic simulation tools based on Gaussian processes',
+      url='https://github.com/atomisticnet/??',
+      version=version,
+      license='LGPLv2.1+',
+      packages=packages,
+      install_requires=install_requires,
+      entry_points={'console_scripts': ['gprpytorch = GPRPytorch.gprpytorch:main', ], },
+)
