c255af66383ad42982d08e726e5045ed1dc4053c
In Won Yeu <yiw0121@snu.ac.kr>
2025-05-14 22:16:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,23 +13,26 @@ packages = ['aenet_GPR',
             'aenet_GPR.src',
             'aenet_GPR.util',]
 
-scripts = ['scripts/aenet_GPR.py', ]
-
 if __name__ == '__main__':
 
     assert sys.version_info >= (3, 0), 'python>=3 is required'
 
-    with open('__init__.py', 'r') as init_file:
+    with open('./README.md', 'rt', encoding='UTF8') as f:
+        long_description = f.read()
+
+    with open('aenet_GPR/__init__.py', 'r') as init_file:
         for line in init_file:
             if "__version__" in line:
                 version = line.split()[2].strip('\"')
                 break
 
 setup(name='aenet_gpr',
-      description='Atomistic simulation tools based on Gaussian processes',
-      url='https://github.com/atomisticnet/aenet_gpr',
       version=version,
+      description='Atomistic simulation tools based on Gaussian processes',
+      long_description=long_description,
+      url='https://github.com/atomisticnet/aenet_gpr',
       license='MPL-2.0',
       packages=packages,
       install_requires=install_requires,
+      python_requires='>=3',
 )
