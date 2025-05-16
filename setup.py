fb4a9c8b7ae194b720cdfccd64a9b6ff9ef2840a
In Won Yeu <yiw0121@snu.ac.kr>
2025-05-16 18:18:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ if __name__ == '__main__':
 
     assert sys.version_info >= (3, 0), 'python>=3 is required'
 
+    with open('./README.md', 'rt', encoding='UTF8') as f:
+        long_description = f.read()
+
     with open('aenet_gpr/__init__.py', 'r') as init_file:
         for line in init_file:
             if "__version__" in line:
@@ -16,6 +19,8 @@ if __name__ == '__main__':
 setup(name='aenet-gpr',
       version=version,
       description='Atomistic simulation tools based on Gaussian Processes Regression',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='In Won Yeu',
       author_email='iy2185@columbia.edu',
       url='https://github.com/atomisticnet/aenet-gpr',
