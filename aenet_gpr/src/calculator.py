d28ac26fcc8aad7f583116bb3d3ea2f9b68c443a
In Won Yeu <yiw0121@snu.ac.kr>
2025-05-26 17:33:01 -0400

    Add files via upload

--- aenet_gpr/src/calculator.py
+++ aenet_gpr/src/calculator.py
@@ -6,11 +6,11 @@ class GPRCalculator(Calculator):
 
     implemented_properties = ['energy', 'forces']
 
-    def __init__(self, calculator, train_energy, **kwargs):
+    def __init__(self, calculator, train_data, **kwargs):
         Calculator.__init__(self, **kwargs)
 
         self.calculator = calculator
-        self.train_energy = train_energy
+        self.train_data = train_data
         self.results = {}
 
     def calculate(self, atoms=None,
@@ -25,17 +25,18 @@ class GPRCalculator(Calculator):
         Calculator.calculate(self, atoms, properties, system_changes)
 
         pred, _ = self.calculator.eval_data_per_data(eval_image=atoms)
-        energy_gpr_scale = pred[0]
-        force_gpr_scale = pred[1:].view(len(atoms), 3)
+        energy_gpr = pred[0]
+        force_gpr = pred[1:].view(len(atoms), 3)
 
-        mean_energy = np.mean(self.train_energy)
-        std_energy = np.std(self.train_energy)
+        if self.train_data.standardization:
+            mean_energy = np.mean(self.train_data.energy)
+            std_energy = np.std(self.train_data.energy)
 
-        # Restore Energy: scaled_energy_target * std + mean
-        energy_gpr = energy_gpr_scale.cpu().detach().numpy() * std_energy + mean_energy
+            # Restore Energy: scaled_energy_target * std + mean
+            energy_gpr = energy_gpr.cpu().detach().numpy() * std_energy + mean_energy
 
-        # Restore Force: scaled_force_target * std
-        force_gpr = force_gpr_scale.cpu().detach().numpy() * std_energy
+            # Restore Force: scaled_force_target * std
+            force_gpr = force_gpr.cpu().detach().numpy() * std_energy
 
         self.results['energy'] = energy_gpr
         self.results['forces'] = force_gpr
