0592146038dfab83d5082ac4305b7db5885b82c4
In Won Yeu <yiw0121@snu.ac.kr>
2025-08-01 23:23:19 -0400

    Add files via upload

--- aenet_gpr/__init__.py
+++ aenet_gpr/__init__.py
@@ -3,13 +3,13 @@ aenet-gpr: A Python package for Gaussian Process Regression (GPR) surrogate mode
 to augment energy data for GPR-ANN potential training
 """
 from .inout import inout_process, input_parameter, io_print, read_input
-from .src import gpr_batch, gpr_iterative, pytorch_kernel, pytorch_kerneltypes, calculator
-from .util import additional_data, param_optimization, prepare_data, reference_data
+from .src import calculator, gpr_batch, gpr_batch_internal, gpr_iterative, prior, pytorch_kernel, pytorch_kernel_internal, pytorch_kerneltypes
+from .util import additional_data, param_optimization, prepare_data, reference_data, reference_data_internal
 from .tool import acquisition, aidneb, ase_tool, trainingset
 
 
-__version__ = "1.8.3"
+__version__ = "1.8.4"
 __all__ = ["inout", "inout_process", "input_parameter", "io_print", "read_input",
-           "src", "gpr_batch", "gpr_iterative", "pytorch_kernel", "pytorch_kerneltypes", "calculator",
-           "util", "additional_data", "param_optimization", "prepare_data", "reference_data",
+           "src", "calculator", "gpr_batch", "gpr_batch_internal", "gpr_iterative", "prior", "pytorch_kernel", "pytorch_kernel_internal", "pytorch_kerneltypes",
+           "util", "additional_data", "param_optimization", "prepare_data", "reference_data", "reference_data_internal",
            "tool", "acquisition", "aidneb", "ase_tool", "trainingset"]
