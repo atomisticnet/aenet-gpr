cf0547ad76d052109cd18dae8bf5d77983086fe6
In Won Yeu <yiw0121@snu.ac.kr>
2025-09-26 14:01:12 -0400

    Add files via upload

--- aenet_gpr/__init__.py
+++ aenet_gpr/__init__.py
@@ -8,7 +8,7 @@ from .util import additional_data, param_optimization, prepare_data, reference_d
 from .tool import acquisition, aidneb, ase_tool, trainingset
 
 
-__version__ = "2.1.7"
+__version__ = "2.1.8"
 __all__ = ["inout", "inout_process", "input_parameter", "io_print", "read_input",
            "src", "calculator", "gpr_batch", "gpr_batch_internal", "gpr_iterative", "prior", "pytorch_kernel", "pytorch_kernel_internal", "pytorch_kerneltypes",
            "util", "additional_data", "param_optimization", "prepare_data", "reference_data", "reference_data_internal",
