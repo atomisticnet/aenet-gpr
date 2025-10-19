57c471be768ed8713dbf33fab34c797611eb36de
In Won Yeu <yiw0121@snu.ac.kr>
2025-10-19 00:42:04 -0400

    Add files via upload

--- aenet_gpr/__main__.py
+++ aenet_gpr/__main__.py
@@ -25,6 +25,7 @@ def main():
     # if input_param.verbose:
     #     io_input_reading(input_param)
 
+    # 1. Train
     if len(input_param.train_file) != 0:
         io_print_title(text='Train')
 
@@ -36,6 +37,7 @@ def main():
         if input_param.train_write:
             train_process.write_reference_train_xsf()
 
+    # 2. Test
     if len(input_param.test_file) != 0:
         io_print_title(text='Test')
 
@@ -44,10 +46,12 @@ def main():
             test_process.load_train_model(train_data=train_process.train_data)
         else:
             test_process.load_train_model(train_data=None)
+
         test_process.read_reference_test_data(structure_files=input_param.test_file,
                                               file_format=input_param.file_format)
         test_process.model_test_evaluation()
 
+    # 3. Augment
     if input_param.additional_write:
         io_print_title(text='Augmentation')
 
