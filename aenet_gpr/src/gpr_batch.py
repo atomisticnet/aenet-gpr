26bc4eb539b81ad6ec0181681ab9868eec9d4f09
In Won Yeu <yiw0121@snu.ac.kr>
2025-10-28 12:47:33 -0400

    Add files via upload

--- aenet_gpr/src/gpr_batch.py
+++ aenet_gpr/src/gpr_batch.py
@@ -554,8 +554,8 @@ class GaussianProcess(object):
                                                                             atoms_mask=self.atoms_mask,
                                                                             delta=self.mace_param.get("delta"),
                                                                             dtype=self.torch_data_type)
-                    fp.append(fp__)
-                    dfp_dr.append(dfp_dr__)
+                    fp.append(torch.tensor(fp__, dtype=self.torch_data_type, device=self.device))
+                    dfp_dr.append(torch.tensor(dfp_dr__, dtype=self.torch_data_type, device=self.device))
 
                 else:
                     fp__, dfp_dr__ = chebyshev_descriptor_gradient(image,
@@ -563,8 +563,8 @@ class GaussianProcess(object):
                                                                    atoms_mask=self.atoms_mask,
                                                                    delta=self.mace_param.get("delta"),
                                                                    dtype=self.torch_data_type)
-                    fp.append(fp__)
-                    dfp_dr.append(dfp_dr__)
+                    fp.append(torch.tensor(fp__, dtype=self.torch_data_type, device=self.device))
+                    dfp_dr.append(torch.tensor(dfp_dr__, dtype=self.torch_data_type, device=self.device))
 
             fp = torch.stack(fp).to(dtype=self.torch_data_type,
                                     device=self.device)  # (Ndata, Nreduced_atoms, Ndescriptor)
