ad3a9f0d19738a2a6072150aaa3c497de9addfbd
In Won Yeu <yiw0121@snu.ac.kr>
2025-11-12 23:35:47 -0500

    Add files via upload

--- aenet_gpr/tool/aidneb.py
+++ aenet_gpr/tool/aidneb.py
@@ -333,7 +333,7 @@ class AIDNEB:
             dt=0.1,
             ml_steps=100,
             optimizer="MDMin",
-            update_step=2,
+            update_step=1,
             check_ref_force=False,
             climbing=False):
 
